8a074f20cea42c62c8801690409ad810195c2fa1
Stefano Sette <ssette@codearchitects.com>
2024-02-16 23:53:59 +0100

    step1: stage test

--- src/app/location.service.ts
+++ src/app/location.service.ts
@@ -1,33 +1,45 @@
-import { Injectable } from '@angular/core';
-import {WeatherService} from "./weather.service";
+import { Injectable, signal } from "@angular/core";
+import { WeatherService } from "./weather.service";
+import { BehaviorSubject } from "rxjs";
 
-export const LOCATIONS : string = "locations";
+export const LOCATIONS: string = "locations";
 
 @Injectable()
 export class LocationService {
+  private _locations = new BehaviorSubject<string[]>([]);
+  public locations = this._locations.asObservable();
 
-  locations : string[] = [];
-
-  constructor(private weatherService : WeatherService) {
+  constructor() {
     let locString = localStorage.getItem(LOCATIONS);
-    if (locString)
-      this.locations = JSON.parse(locString);
-    for (let loc of this.locations)
-      this.weatherService.addCurrentConditions(loc);
+    if (locString) this._locations.next(JSON.parse(locString));
   }
 
-  addLocation(zipcode : string) {
-    this.locations.push(zipcode);
-    localStorage.setItem(LOCATIONS, JSON.stringify(this.locations));
-    this.weatherService.addCurrentConditions(zipcode);
+  addLocation(zipcode: string) {
+    const current = this._locations.value;
+    if (current.indexOf(zipcode) !== -1) return; // no duplicates
+    this._locations.next([...current, zipcode]);
+    localStorage.setItem(LOCATIONS, JSON.stringify(this._locations.value));
   }
 
-  removeLocation(zipcode : string) {
-    let index = this.locations.indexOf(zipcode);
-    if (index !== -1){
-      this.locations.splice(index, 1);
-      localStorage.setItem(LOCATIONS, JSON.stringify(this.locations));
-      this.weatherService.removeCurrentConditions(zipcode);
+  removeLocation(zipcode: string) {
+    const current = this._locations.value;
+    let index = current.indexOf(zipcode);
+    if (index !== -1) {
+      this._locations.next([
+        ...current.slice(0, index),
+        ...current.slice(index + 1),
+      ]);
+      localStorage.setItem(LOCATIONS, JSON.stringify(this._locations.value));
+    }
+  }
+
+  removeLocations(zipcodes: string[]) {
+    const updated = this._locations.value.filter(
+      (location) => zipcodes.indexOf(location) === -1
+    );
+    if (updated.length !== this._locations.value.length) {
+      this._locations.next(updated);
+      localStorage.setItem(LOCATIONS, JSON.stringify(this._locations.value));
     }
   }
 }
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,35 +1,143 @@
-import {Injectable, Signal, signal} from '@angular/core';
-import {Observable} from 'rxjs';
+import { Injectable, Signal, inject, signal } from "@angular/core";
+import { Observable, Subject, forkJoin, of, zip } from "rxjs";
+import {
+  catchError,
+  finalize,
+  map,
+  mergeMap,
+  switchMap,
+  takeUntil,
+} from "rxjs/operators";
 
-import {HttpClient} from '@angular/common/http';
-import {CurrentConditions} from './current-conditions/current-conditions.type';
-import {ConditionsAndZip} from './conditions-and-zip.type';
-import {Forecast} from './forecasts-list/forecast.type';
+import { HttpClient } from "@angular/common/http";
+import { CurrentConditions } from "./current-conditions/current-conditions.type";
+import { ConditionsAndZip } from "./conditions-and-zip.type";
+import { Forecast } from "./forecasts-list/forecast.type";
+import { LocationService } from "./location.service";
+import { takeUntilDestroyed, toObservable } from "@angular/core/rxjs-interop";
+import { ToastrService } from "ngx-toastr";
 
 @Injectable()
 export class WeatherService {
-
-  static URL = 'http://api.openweathermap.org/data/2.5';
-  static APPID = '5a4b2d457ecbef9eb2a71e480b947604';
-  static ICON_URL = 'https://raw.githubusercontent.com/udacity/Sunshine-Version-2/sunshine_master/app/src/main/res/drawable-hdpi/';
+  static URL = "http://api.openweathermap.org/data/2.5";
+  static APPID = "5a4b2d457ecbef9eb2a71e480b947604";
+  static ICON_URL =
+    "https://raw.githubusercontent.com/udacity/Sunshine-Version-2/sunshine_master/app/src/main/res/drawable-hdpi/";
   private currentConditions = signal<ConditionsAndZip[]>([]);
+  /**
+   * Map which holds the zipcode of each request and the subject to cancel the request
+   */
+  private _queue = new Map<string, Subject<void>>();
 
-  constructor(private http: HttpClient) { }
-
-  addCurrentConditions(zipcode: string): void {
-    // Here we make a request to get the current conditions data from the API. Note the use of backticks and an expression to insert the zipcode
-    this.http.get<CurrentConditions>(`${WeatherService.URL}/weather?zip=${zipcode},us&units=imperial&APPID=${WeatherService.APPID}`)
-      .subscribe(data => this.currentConditions.update(conditions => [...conditions, {zip: zipcode, data}]));
+  constructor(
+    private http: HttpClient,
+    private locationService: LocationService,
+    private toastr: ToastrService
+  ) {
+    this.locationService.locations
+      .pipe(
+        mergeMap((locations) => this._locationsToConditions(locations)),
+        takeUntilDestroyed() // safe since we are within the main injection context
+      )
+      .subscribe((locations) => {
+        this.currentConditions.set(
+          locations.filter((c) => c.data !== undefined)
+        );
+        const error = locations.filter((c) => c.data === undefined);
+        this._cancel(error);
+        if (error.length) {
+          // removing any location that gave error
+          this.locationService.removeLocations(error.map((loc) => loc.zip));
+        }
+      });
   }
-
-  removeCurrentConditions(zipcode: string) {
-    this.currentConditions.update(conditions => {
-      for (let i in conditions) {
-        if (conditions[i].zip == zipcode)
-          conditions.splice(+i, 1);
+  /**
+   * Maps the locations to multiple conditions observables
+   * @param locations
+   * @returns
+   */
+  private _locationsToConditions(
+    locations: string[]
+  ): Observable<ConditionsAndZip[]> {
+    const current = this.currentConditions();
+    const toReplay: string[] = [];
+    const toAdd: string[] = [];
+    for (const loc of locations) {
+      if (current.findIndex((c) => c.zip === loc) === -1) {
+        toAdd.push(loc);
+      } else {
+        toReplay.push(loc);
       }
-      return conditions;
-    })
+    }
+    const obs$: Observable<ConditionsAndZip>[] = [];
+
+    for (const loc of toReplay) {
+      // replay the current conditions avoiding making a new request
+      obs$.push(
+        this._queueRequest(loc, of(current.find((c) => c.zip === loc)))
+      );
+    }
+
+    for (const loc of toAdd) {
+      obs$.push(this._queueRequest(loc, this._getConditions(loc)));
+    }
+
+    // others should be unsubscribed since request could still be alive or actually never requested
+    const toBeCanceled = current.filter((c) => toReplay.indexOf(c.zip) === -1);
+    this._cancel(toBeCanceled);
+    if (obs$.length === 0) return of([]);
+
+    return forkJoin(obs$);
+  }
+  /**
+   * Returns a request which gets queued and can be canceled later
+   */
+  private _queueRequest(
+    zip: string,
+    request: Observable<ConditionsAndZip>
+  ): Observable<ConditionsAndZip> {
+    const cancel$ = new Subject<void>();
+    this._queue.set(zip, cancel$);
+    return request.pipe(
+      takeUntil(cancel$),
+      finalize(() => this._queue.delete(zip))
+    );
+  }
+  /**
+   * Cancels any ongoing request for the given conditions
+   */
+  private _cancel(conditions: ConditionsAndZip[]) {
+    for (const condition of conditions) {
+      // cancel the request if still ongoing
+      const cancel$ = this._queue.get(condition.zip);
+      if (cancel$) {
+        cancel$.next();
+        this._queue.delete(condition.zip);
+      }
+    }
+  }
+  /**
+   * Makes http request to get the current conditions
+   * @param zipcode Location to get the conditions
+   */
+  private _getConditions(zipcode: string): Observable<ConditionsAndZip> {
+    return this.http
+      .get<CurrentConditions>(
+        `${WeatherService.URL}/weather?zip=${zipcode},us&units=imperial&APPID=${WeatherService.APPID}`
+      )
+      .pipe(
+        catchError((err) => {
+          if (err.status === 404)
+            this.toastr.error(`Location ${zipcode} not found`, "Error");
+          else
+            this.toastr.error(
+              `An error occurred while loading ${zipcode} location`,
+              "Error"
+            );
+          return of(undefined);
+        }),
+        map((data) => ({ zip: zipcode, data }))
+      );
   }
 
   getCurrentConditions(): Signal<ConditionsAndZip[]> {
@@ -38,8 +146,9 @@ export class WeatherService {
 
   getForecast(zipcode: string): Observable<Forecast> {
     // Here we make a request to get the forecast data from the API. Note the use of backticks and an expression to insert the zipcode
-    return this.http.get<Forecast>(`${WeatherService.URL}/forecast/daily?zip=${zipcode},us&units=imperial&cnt=5&APPID=${WeatherService.APPID}`);
-
+    return this.http.get<Forecast>(
+      `${WeatherService.URL}/forecast/daily?zip=${zipcode},us&units=imperial&cnt=5&APPID=${WeatherService.APPID}`
+    );
   }
 
   getWeatherIcon(id): string {
@@ -55,8 +164,6 @@ export class WeatherService {
       return WeatherService.ICON_URL + "art_clouds.png";
     else if (id === 741 || id === 761)
       return WeatherService.ICON_URL + "art_fog.png";
-    else
-      return WeatherService.ICON_URL + "art_clear.png";
+    else return WeatherService.ICON_URL + "art_clear.png";
   }
-
 }
